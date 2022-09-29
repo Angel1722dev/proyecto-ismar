b32a0f1dd78a4328f232e0fb14a9f587af4b31c7
Angel1722dev <angel1722.net@gmail.com>
2022-09-29 09:49:32 -0400

    tercer commit,hola mundo

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
 const saludo = "hola mundo";
 console.log(saludo);
+
+const sumar = (v1, v2) => {
+  const result = v1 + v2;
+  return result;
+};
+
+const mostrar = sumar(1, 10);
+console.log(mostrar);
