0129220e2ac02aa329a66b33bb11fe4dc5e4bb9a
Angel1722dev <angel1722.net@gmail.com>
2022-09-29 10:17:21 -0400

    agregar funciones

--- src/app.js
+++ src/app.js
@@ -7,4 +7,19 @@ const sumar = (v1, v2) => {
 };
 
 const mostrar = sumar(1, 10);
-console.log(mostrar);
+//console.log(mostrar);
+
+const objeto = {
+  nombre: "angel",
+  apellido: "Briceño",
+  edad: "39",
+  nacionalidad: "venezolana",
+  suma: sumar(5, 10),
+};
+
+const arreglo = [sumar(10, 10), saludo, objeto, 1, 2, 3, 4, 5, 6];
+
+const objeto1 = {
+  arreglo,
+};
+console.log(objeto1);
